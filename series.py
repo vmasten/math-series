65e0b3ead305638767425fd16f5547ccf7dc0ab1
Vince Masten <vmasten@me.com>
2018-11-27 21:44:53 -0800

    functionally complete

--- series.py
+++ series.py
@@ -1,6 +1,8 @@
 import sys
 
 def fibonacci(n):
+  """
+  """
   if type(n) is not int:
     raise TypeError('Argument must be an integer')
     # Because lucas() relies on fibonacci() input only needs to be validated here
@@ -37,28 +39,53 @@ if __name__ == "__main__":
   print('This module defines functions that implement mathematical series')
   print('...')
   print('if at any time you wish to exit, type "quit" or use the keyboard shortcut control-c')
+
   print('\nfibonacci(n):\n')
   print('Returns the nth value in the fibonacci series\n')
+
   print('lucas(n):\n')
   print('Returns the nth value in the lucas number series\n')
-  print('sum_series(n):\n')
-  print('If called with one parameter, returns the nth value in the fibonacci series;\nif called with optional parameters 2 and 1, returns the nth value in the lucas number series\n')
+
+  print('sum_series(n): or sum_series(n, x, y)\n')
+  print('If called with one parameter, returns the nth value in the fibonacci series;\nif called with optional parameters 2 and 1 (comma-separated), returns the nth value in the lucas number series\n')
+
+  print('Any other command will be ignored\n')
 
   try:
     while True:
       prompt = input()
+
       if prompt == 'quit':
         sys.exit()
+
       prompt = prompt.split('(')
       if prompt[0] == 'fibonacci':
         n = prompt[1].split(')')
-        print(fibonacci(n = int(n[0])), '\n')
+        try:
+          print(fibonacci(n = int(n[0])), '\n')
+        except ValueError:
+          print('Input must be an integer')
+
       elif prompt[0] == 'lucas':
         n = prompt[1].split(')')
-        print(lucas(n = int(n[0])), '\n')
+        try:
+          print(lucas(n = int(n[0])), '\n')
+        except ValueError:
+          print('Input must be an integer\n')
+
       elif prompt[0] == 'sum_series':
         n = prompt[1].split(')')
-        print(sum_series(n = int(n[0])), '\n')
+        try:
+          if n[0][1] == ',':
+            a, b, c = n[0].split(',')
+          try:
+            print(sum_series(int(a), int(b), int(c)))
+          except ValueError:
+              print('Input must be an integer(s)\n')
+          except NameError:
+              print('Input must be an integer(s)\n')
+        except IndexError:
+          print(sum_series(n = int(n[0])), '\n')
 
   except KeyboardInterrupt:
       exit()
